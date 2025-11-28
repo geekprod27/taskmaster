06d95daca0b02df2c80fc9cf6fb58e326ecf6aeb
nathan <natfeljaf@gmail.com>
2025-11-28 17:19:55 +0100

    wip: creating makefile and config.hxx

---
+++ includes/config.hxx
@@ -0,0 +1,13 @@
+typedef struct {
+std::string	m_start_command;
+uint8_t m_how_many_processes;
+bool m_has_to_be_started_at_launch;
+std::vector<uint8_t> m_expected_exit_status;
+
+} t_program_rules
+
+typedef struct {
+
+
+
+} t_config;
