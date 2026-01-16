4b89b6a87b1510b23632bccb03a8471e100d31dc
Jonathan DUFOUR <jonathan.dufour@etik.com>
2026-01-16 18:26:33 +0100

    fix: changed redirection to non-optional
    It has been decided that the parsing will be responsible
    to provide a default path whenever the configuration file
    does not provide an explicit one for either the stdout
    or stderr redirection. In this optic, it is more relevant
    to store an `AbsolutePath` instead of an `std::optional`.

--- include/type/program_rules.hxx
+++ include/type/program_rules.hxx
@@ -14,20 +14,20 @@
 namespace taskmaster {
 
 struct ProgramRules {
-    Command                     m_start_command;
-    ProcessCounter              m_how_many_processes;
-    bool                        m_must_be_started_at_launch;
-    RestartOnExitRule           m_restart_on_exit_rule;
-    std::vector<ExitStatus>     m_expected_exit_status;
-    std::chrono::milliseconds   m_successful_start_time;
-    RestartAttemptCounter       m_how_many_restart_attempts; // before aborting
-    Signal                      m_graceful_stop_signal;
-    std::chrono::milliseconds   m_graceful_stop_time;
-    std::optional<AbsolutePath> m_stdout_redirection_file;
-    std::optional<AbsolutePath> m_stderr_redirection_file;
-    Environment                 m_environment;
-    AbsolutePath                m_working_directory;
-    mode_t                      m_permissions_on_new_files;
+    Command                   m_start_command;
+    ProcessCounter            m_how_many_processes;
+    bool                      m_must_be_started_at_launch;
+    RestartOnExitRule         m_restart_on_exit_rule;
+    std::vector<ExitStatus>   m_expected_exit_status;
+    std::chrono::milliseconds m_successful_start_time;
+    RestartAttemptCounter     m_how_many_restart_attempts; // before aborting
+    Signal                    m_graceful_stop_signal;
+    std::chrono::milliseconds m_graceful_stop_time;
+    AbsolutePath              m_stdout_redirection_file;
+    AbsolutePath              m_stderr_redirection_file;
+    Environment               m_environment;
+    AbsolutePath              m_working_directory;
+    mode_t                    m_permissions_on_new_files;
 };
 
 } // namespace taskmaster
