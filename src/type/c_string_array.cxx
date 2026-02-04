a0acef442902da54146ebbeaf6258cd66ed3399d
Jonathan DUFOUR <jonathan.dufour@etik.com>
2026-02-04 04:23:31 +0100

    fix: explicitly initialized the trailing `nullptr` in CStringArray

--- src/type/c_string_array.cxx
+++ src/type/c_string_array.cxx
@@ -1,8 +1,5 @@
 #include "type/c_string_array.hxx"
-#include <algorithm>
-#include <cstring>
 #include <utility>
-#include <vector>
 
 namespace taskmaster {
 
@@ -22,11 +19,11 @@ CStringArray::CStringArray(
         }
         tmp.emplace_back(c_string, free);
     }
-    std::transform(
+    *std::transform(
         tmp.begin(), tmp.end(), m_array.get(), [](std::unique_ptr<char, void (*)(void *)> &ptr) {
             return ptr.release();
         }
-    );
+    ) = nullptr;
 }
 
 CStringArray::CStringArray(
@@ -34,30 +31,6 @@ CStringArray::CStringArray(
 ) noexcept
 : m_size(std::exchange(other.m_size, 0)), m_array(std::move(other.m_array))
 {}
-
-template <typename TP_Container>
-CStringArray::CStringArray(
-    TP_Container const &strings
-)
-: m_size(strings.size()), m_array(m_size ? std::make_unique<char *[]>(m_size + 1) : nullptr)
-{
-    std::vector<std::unique_ptr<char, void (*)(void *)>> tmp;
-
-    tmp.reserve(m_size);
-    for (typename TP_Container::value_type const &string : strings) {
-        char *const c_string = strdup(string.c_str());
-
-        if (c_string == nullptr) {
-            throw std::bad_alloc();
-        }
-        tmp.emplace_back(c_string, free);
-    }
-    std::transform(
-        tmp.begin(), tmp.end(), m_array.get(), [](std::unique_ptr<char, void (*)(void *)> &ptr) {
-            return ptr.release();
-        }
-    );
-}
 
 CStringArray::~CStringArray() noexcept
 {
